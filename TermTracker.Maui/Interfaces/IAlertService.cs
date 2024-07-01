960a051423b27a053a735dd4d8267a65dd559307
Lorenzo Ortiz <LorenzoDavidOrtiz@gmail.com>
2024-06-30 17:54:22 -0700

    Part D: Updates wireframe

--- TermTracker.Maui/Interfaces/IAlertService.cs
+++ TermTracker.Maui/Interfaces/IAlertService.cs
@@ -2,6 +2,6 @@
 public interface IAlertService
 {
     Task ShowToast(string text);
-:     Task ScheduleLocalNotification(int alertId, string title, string description, DateTime targetNotifyTime);
+    Task ScheduleLocalNotification(int alertId, string title, string description, DateTime targetNotifyTime);
     public bool CancelLocalNotification(int alertId);
 }
